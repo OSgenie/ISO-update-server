a1b402cfe293f96b25dcafeb6b1653ff59d7e93f
kirtley <kirtley@kirtley-zbox>
2013-02-03 09:49:31 -0800

    .

--- run-updaters.sh
+++ run-updaters.sh
@@ -4,15 +4,20 @@ available_updaters=$(virsh list --all --name | grep updater)
 already_updated=()
 next_updaters=()
 active_updater=$(virsh list --name | grep updater)
-if [ $active_updater == "" ]; then 
-    virsh start ${available_updaters[0]}
-fi
-for (( i=0;i<${#available_updaters[@]};i++)); do
+server=0
+
+echo "Available updaters - ${available_updaters[@]}"
+echo "Active updater - $active_updater"
+for i in {1..2000}; do
     active_updater=$(virsh list --name | grep updater)
-    while [ active_updater != "" ]; do
-        sleep 300
-        active_updater=$(virsh list --name | grep updater)
-    done
-        virsh start ${available_updaters[$i]}
-        unset ${available_updaters[$i]}
+    if [ active_updater != "" ]; do
+        echo "${available_updaters[$server]}"
+        virsh start ${available_updaters[$server]}
+        server=$((server++))
+    fi
+    if [ $server > ${#available_updaters[@]}]; then
+        echo "All Updaters have run"
+        exit
+    fi
+    sleep 300
 done
