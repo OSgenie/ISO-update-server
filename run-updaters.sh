b5d309825b1b8d0de42c4dfcd8f1f9ba0a50fceb
kirtley <kirtley@kirtley-zbox>
2013-02-03 09:56:04 -0800

    .

--- run-updaters.sh
+++ run-updaters.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # set crontab to run weekly
 available_updaters=$(virsh list --all --name | grep updater)
-already_updated=()
-next_updaters=()
-active_updater=$(virsh list --name | grep updater)
 server=0
 
 function check_for_sudo ()
@@ -17,20 +14,21 @@ fi
 function run_updaters ()
 {
 echo "Available updaters - ${available_updaters[@]}"
-echo "Active updater - $active_updater"
 for i in {1..2000}; do
+echo "Active updater - $active_updater"
     active_updater=$(virsh list --name | grep updater)
     if [ active_updater != "" ]; then
-        echo "${available_updaters[$server]}"
+        echo ${available_updaters[$server]}
         virsh start ${available_updaters[$server]}
         server=$((server++))
     fi
-    if [ $server >= ${#available_updaters[@]}]; then
+    if [ $server >= ${#available_updaters[@]} ]; then
         echo "All Updaters have run"
         exit
     fi
     sleep 300
 done
 }
+
 check_for_sudo
 run_updaters
