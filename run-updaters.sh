55d0e714ec79df82435bd5e0e1a65ed48e8ce4f2
kirtley <kirtley@kirtley-zbox>
2013-02-04 01:58:09 -0800

    .

--- run-updaters.sh
+++ run-updaters.sh
@@ -2,7 +2,7 @@
 # set crontab to run weekly
 available_updaters=()
 available_updaters=(${available_updaters[@]} $(virsh list --all --name | grep updater))
-server=0
+updater=0
 
 function check_for_sudo ()
 {
@@ -17,26 +17,26 @@ function run_updaters ()
 echo "+-------------------------------------------------------------------+"
 echo "+ Starting Weekly Update with" 
 echo "+ ${available_updaters[@]}"
-echo $server
+echo $updater
 echo "+-------------------------------------------------------------------+"
 for i in {1..2000}; do
     active_updater=$(virsh list --name | grep updater)
     echo "Active updater - $active_updater"
     if [ "$active_updater" == "" ]; then
-        if [ $server == ${#available_updaters[@]} ]; then
+        if [ $updater == ${#available_updaters[@]} ]; then
             echo "+-------------------------------------------------------------------+"
             echo "All Update Servers have run"
             echo "+-------------------------------------------------------------------+"
             exit
         else
-            echo $server            
+            echo $updater            
             echo "+-------------------------------------------------------------------+"    
             echo "+ RUNNING ${available_updaters[$server]}"
             echo "+ `date +%c`"
             echo "+-------------------------------------------------------------------+"    
             virsh start ${available_updaters[$server]}
-            server=$((server+1))
-            echo $server
+            updater=$((updater+1))
+            echo $updater
         fi
     fi
     sleep 30
