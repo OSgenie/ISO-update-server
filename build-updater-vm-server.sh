f3ff7290ab3ea29d670236fda9c8cb23b0b10b35
kirtley <kirtley@kirtley-zbox>
2013-02-06 11:37:59 -0800

    .

--- build-updater-vm-server.sh
+++ build-updater-vm-server.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+function check_for_sudo ()
+{
+if [ $UID != 0 ]; then
+		echo "You need root privileges"
+		exit 2
+fi
+}
+
+function build_updater_environment ()
+{
 $scriptdir/install-KVM.sh
 $scriptdir/buildvm-updater.sh
 $scriptdir/install-run-updater-to-crontab.sh
+}
+
+check_for_sudo
+build_updater_environment
